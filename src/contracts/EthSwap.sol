b4ec38ecbed1178dc688441009fd2e60417afa16
Srinjay-hack <srinjaymondal@gmail.com>
2021-12-05 13:23:08 +0530

    sellTokens

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -8,7 +8,15 @@ contract EthSwap{
     uint public rate =100;
 
 
-    event TokenPurchased(
+    event TokensPurchased(
+        address account,
+        address token,
+        uint amount,
+        uint rate
+    );
+
+
+    event TokensSold(
         address account,
         address token,
         uint amount,
@@ -23,9 +31,34 @@ contract EthSwap{
         //Redemption rate =NO of tokens they receive for 1 ether
         //Amount of Etherum*Redemption rate
         uint tokenAmount = msg.value * rate;
+        require(token.balanceOf(address(this)) >= tokenAmount);
+
         token.transfer(msg.sender,tokenAmount);
 
         //Emit an event
-        emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+        emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
+
+
+
+    function sellTokens(uint _amount) public {
+
+        //User cant sell more tokens than they have
+        require(token.balanceOf(msg.sender) >= _amount);
+        
+        //Calculate the amount of Ether to redeem
+        uint etherAmount =_amount/rate;
+
+        //Require that EthSwap has enough Ether
+        require(address(this).balance >= etherAmount);
+        
+
+        // Perform sale
+        token.transferFrom(msg.sender,address(this), _amount);
+        msg.sender.transfer(etherAmount);
+
+        //emit an Event
+
+        emit TokensSold(msg.sender,address(token), _amount,rate);
     }
 }
